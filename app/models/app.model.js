04d127716aad844b556b60e2c9126c71e978162d
PeishanS <peishan.s68@gmail.com>
2025-04-28 22:43:27 +0100

    4-get-articles passed tests

--- app/models/app.model.js
+++ app/models/app.model.js
@@ -12,7 +12,6 @@ exports.selectArticleById = (article_id) => {
     return db
     .query(`SELECT * FROM articles WHERE article_id = $1`,[article_id])
     .then(({rows}) => {
-        console.log(rows, "<<<<<<<<<<<<")
         if(rows.length === 0){
             return Promise.reject({
                 status: 404,
@@ -20,5 +19,25 @@ exports.selectArticleById = (article_id) => {
             })
         }
         return rows[0];
+    })
+}
+
+exports.selectArticles = () => {
+    return db
+    .query(`SELECT
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id)::INT AS comment_count
+        FROM articles
+        LEFT OUTER JOIN comments ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;`)
+    .then(({rows}) => {
+        return rows;
     })
 }
