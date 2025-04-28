7b75a76b4ff238bac4a96a7211cb51e6e5f74ba6
PeishanS <peishan.s68@gmail.com>
2025-04-28 14:24:53 +0100

    2-get-topics passed test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,7 @@ beforeEach(() => seed(data));
 afterAll(() => db.end());
 
 describe("GET /api", () => {
-  test.only("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
@@ -18,3 +18,19 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe.only("GET /api/topics", () => {
+  test("status 200 - responds with the requested array containing all of the topics", () => {
+    return request(app)
+    .get("/api/topics")
+    .expect(200)
+    .then(({body}) => {
+      expect(body.topics).toHaveLength(3);
+      body.topics.forEach((topic) => {
+        expect(typeof topic.description).toBe("string");
+        expect(typeof topic.slug).toBe("string");
+        expect(topic).not.toHaveProperty("img_url");
+      })
+    })
+  })
+})
