6cb1ac7cb33c497e65226d1c63408de56d94ad62
PeishanS <peishan.s68@gmail.com>
2025-05-02 11:47:02 +0100

    11-get-articles-topic-query passed tests

--- app/controllers/app.controller.js
+++ app/controllers/app.controller.js
@@ -24,9 +24,9 @@ exports.getArticleById = (req, res, next) => {
 }
 
 exports.getArticles = (req, res,next) => {
-    const {sort_by, order } = req.query;
+    const {sort_by, order, topic } = req.query;
 
-    return selectArticles(sort_by, order)
+    return selectArticles(sort_by, order, topic)
     .then((articles) => {
         res.status(200).send({articles})
     })
